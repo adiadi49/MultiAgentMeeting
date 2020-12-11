e88d2523182a2affcdba898c23fffa3c84d08b40
kashyap999-m <60240540+kashyap999-m@users.noreply.github.com>
2020-12-11 16:54:25 -0700

    Add files via upload

---
+++ src/jadeproject/Calendar.java
@@ -0,0 +1,42 @@
+package jadeproject;
+
+import java.util.ArrayList;
+
+
+public class Calendar {
+    private ArrayList<Day> days;
+
+    public Calendar() {
+        this.setDays(new ArrayList<>());
+        this.initDays();
+    }
+
+    private void initDays() {
+        for (int i = 1; i <= Settings.numDays; i++) {
+            this.getDays().add(new Day(i));
+        }
+    }
+
+    public ArrayList<Day> getDays() {
+        return days;
+    }
+
+    public void setDays(ArrayList<Day> days) {
+        this.days = days;
+    }
+
+    public Day getDaybyNum(int num) {
+        for(Day d : days)
+            if (d.getNumberOfDay() == num)
+                return d;
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        for (Day d: days)
+            s += d.toString() + "\n";
+        return s;
+    }
+}
